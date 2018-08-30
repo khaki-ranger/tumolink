6b5b099535a694d7c3345249407303d6babfe7b9
Yohei Terashima <yamanogomora@gmail.com>
2018-08-30 23:15:11 +0000

    Herokuデプロイ準備

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,7 +1,7 @@
 'use strict';
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  'postgres://postgres:postgres@localhost/tumolink',
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/tumolink',
   { logging: true });
 
 module.exports = {
