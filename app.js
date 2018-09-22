ba1c9739847a6aef35c75d0b100a1673bb038411
Yohei Terashima <yamanogomora@gmail.com>
2018-09-22 03:47:33 +0000

    セッションに保存する時間を指定

--- app.js
+++ app.js
@@ -76,7 +76,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({ secret: '6d1a111ab8df9525', resave: false, saveUninitialized: false }));
+app.use(session({
+  secret: '6d1a111ab8df9525',
+  resave: false,
+  saveUninitialized: false, 
+  cookie:{_expires : 10800000}
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
